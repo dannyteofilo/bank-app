532b76eabcbf8b92f479a21217d8f994f054187b
Teofilo <danyel.285309@gmail.com>
2020-11-19 14:49:14 -0600

    feat: styling sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { ItemSideBar } from './ItemSideBar'
 
 export const SideBar = () => {
+    const items = [
+        {
+            "id": 1,
+            "name": "Quinn Wiley",
+            "bank": 4
+        },
+        {
+            "id": 2,
+            "name": "Josiah Montgomery",
+            "bank": 2
+        },
+        {
+            "id": 3,
+            "name": "Hedley Todd",
+            "bank": 2
+        },
+        {
+            "id": 4,
+            "name": "Victor Boone",
+            "bank": 4
+        }
+    ]
+    const handleEmployees = (e) => {
+        console.log('employee: ', e);
+    }
     return (
         <div className='sideBar__container'>
-            SideBar
+            <div className='main'>
+                <div className='title'>
+                    <h2>Bank App</h2>
+                </div>
+            </div>
+            <div className='items'>
+                {
+                    items.map((item) => (
+                        <ItemSideBar key={item.id} item={item} click={handleEmployees} />
+                    ))
+                }
+            </div>
         </div>
     )
 }
