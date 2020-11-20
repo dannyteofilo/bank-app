210a1ea55489e76af2404d8134a9849bf02e9720
Teofilo <danyel.285309@gmail.com>
2020-11-19 19:49:44 -0600

    feat: styling navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import moment from 'moment'
 
-export const NavBar = () => {
+
+export const NavBar = ({title}) => {
+    const titleDefault = 'Branch'
+    const date = new Date();
     return (
         <div>
-            NavBar
+            <nav className="navbar navbar-dark">
+                <a className="navbar-brand" href="#">{title ? title : titleDefault}</a>
+                <a className="navbar-brand" href="#">{moment(date).format("MMM Do YY")}</a>
+            </nav>
         </div>
     )
 }
