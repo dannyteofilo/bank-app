1197c22f85a5dbf040f43d093e2b7ff63122871b
Teofilo <danyel.285309@gmail.com>
2020-11-19 19:19:29 -0600

    feat: adding form to add employee

--- src/components/EmployeForm.js
+++ src/components/EmployeForm.js
@@ -1,9 +1,70 @@
 import React from 'react'
+import {useForm} from '../hooks/useForm';
 
 export const EmployeForm = () => {
+
+    const [formValues, handleInputChange] = useForm({
+        name: '',
+        middle_name: '',
+        last_name: '',
+        branch: '',
+    });
+
+    const { name ,middle_name ,last_name ,branch } = formValues;
+
+    const handleAdd = (e) => {
+        e.preventDefault();
+        console.log('submit form');
+
+    }
     return (
-        <div>
-            EmployeForm
+        <div className='container'>
+            <form onSubmit={handleAdd}>
+
+            <div className='form-group'>
+                <input
+                    type='text'
+                    placeholder='Name'
+                    name='name'
+                    autoComplete='off'
+                    value={name}
+                    onChange={handleInputChange}
+                />
+                </div>
+                <div className='form-group'>
+                <input
+                    type='text'
+                    placeholder='Middle Name'
+                    name='middle_name'
+                    autoComplete='off'
+                    value={middle_name}
+                    onChange={handleInputChange}
+                />
+                </div>
+                <div className='form-group'>
+                <input
+                    type='text'
+                    placeholder='Last Name'
+                    name='last_name'
+                    autoComplete='off'
+                    value={last_name}
+                    onChange={handleInputChange}
+                />
+                </div>
+                <div className='form-group'>
+                <input
+                    type='text'
+                    placeholder='Branch'
+                    name='branch'
+                    autoComplete='off'
+                    value={branch}
+                    onChange={handleInputChange}
+                />
+                </div>
+                <div className='form-group'>
+                    <button type='submit' className='btn btn-primary mb-2'>Submit</button>
+                </div>
+            </form>
         </div>
     )
 }
