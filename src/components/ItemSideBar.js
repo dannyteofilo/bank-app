4f64c8b6a9cfafb842dc34240acc6fa8c893f502
Teofilo <danyel.285309@gmail.com>
2020-11-19 20:48:08 -0600

    fix: styling item active

--- src/components/ItemSideBar.js
+++ src/components/ItemSideBar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export const ItemSideBar = (props) => {
-    const { id, name, bank } = props.item
+    const { id, name, bank } = props.item;
+    let active=false;
     const handleCkick = () => {
-        props.click(bank)
+        props.click(bank,id)
     }
     return (
-        <div className='row mt-5 pointer link' onClick={handleCkick}>
+        <div className={active ? styles['row mt-5 pointer link active']:['row mt-5 pointer link']}  onClick={handleCkick}>
             <div className='col-1'>
                 <i className="fas fa-coins"></i>
             </div>
