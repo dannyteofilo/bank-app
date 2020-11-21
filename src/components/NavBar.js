660bf23f3c86b7ee9e58efd8f62042977405a0a6
Teofilo <danyel.285309@gmail.com>
2020-11-20 23:30:44 -0600

    feat: link to source code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,10 @@ export const NavBar = () => {
         <div>
             <nav className="navbar navbar-dark">
                 <p className="navbar-brand">{current ? current.title : titleDefault}</p>
+                <div className='col-5'>
+                <a className="navbar-brand" href="https://github.com/dannyteofilo/bank-app"><i class="fab fa-github">Source code</i></a>
                 <p className="navbar-brand">{moment(date).format("MMM Do YY")}</p>
+                </div>
             </nav>
         </div>
     )
